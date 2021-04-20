c995336050bc641ec00a976dddc1eaae75a3e5bb
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-04-20 10:09:02 -0700

    initialized react Application and added about and nav components

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+import React from 'react';
+
+function About() {
+    // return(
+    //     <section>
+    //         <h1 id="about">Who am I?</h1>
+    //     </section>
+    // );
+    return (
+        <section className="my-5">
+        <h1 id="about">Who am I?</h1>
+        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      </section>
+      
+      );
+}
+
+export default About;
